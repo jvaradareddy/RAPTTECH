23f16a723de9804b5d598475432a4e97ba882a05
Varada R <Varadar@SARA.COM>
2025-08-22 14:07:34 +0530

    first commit

--- script.js
+++ script.js
@@ -67,6 +67,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+});
+
 // -----------------------
 // Set initial button state
 // -----------------------
